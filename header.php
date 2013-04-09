07d7b9fcc8d9b106e351d2120f56c79d586aa42a
Josh Pollock <josh@Josh-Pollocks-MacBook-Pro.local>
2013-04-09 16:45:21 -0400

    added foundation.php

--- header.php
+++ header.php
@@ -35,7 +35,28 @@
 				<h1 class="menu-toggle"><?php _e( 'Menu', '_s' ); ?></h1>
 				<div class="screen-reader-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', '_s' ); ?>"><?php _e( 'Skip to content', '_s' ); ?></a></div>
 
-				<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+		<?php
+		$defaults = array(
+			'theme_location'  => 'primary',
+			'menu'            => '',
+			'container'       => 'nav',
+			'container_class' => 'top-bar',
+			'container_id'    => '',
+			'menu_class'      => 'menu',
+			'menu_id'         => '',
+			'echo'            => true,
+			'fallback_cb'     => 'wp_page_menu',
+			'before'          => '',
+			'after'           => '',
+			'link_before'     => '',
+			'link_after'      => '',
+			'items_wrap'      => '<ul class="left">%3$s</ul>',
+			'depth'           => 0,
+			'walker'          => ''
+		);
+		wp_nav_menu( $defaults );
+		?>
+
 			</div>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
