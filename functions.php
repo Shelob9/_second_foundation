47d375b81e49506fd048a3b8abafe17b059f0931
Josh <JPollock412@gmail.com>
2013-04-15 18:40:54 -0400

    move foundation js to footer

--- functions.php
+++ functions.php
@@ -181,7 +181,7 @@ add_action( 'wp_enqueue_scripts', '_s_scripts' );
  */
  function _sf_foundation() {
  	//Foundation scripts/ styles
-	wp_enqueue_script('foundation', get_template_directory_uri().'/js/foundation.min.js', array( 'jquery' ));
+	wp_enqueue_script('foundation', get_template_directory_uri().'/js/foundation.min.js', array( 'jquery' ), false, true);
 	wp_enqueue_script('modernizer', get_template_directory_uri().'/js/custom.modernizr.js');
 	wp_enqueue_style('foundation', get_template_directory_uri().'/css/foundation.min.css');
 	wp_enqueue_style('foundation', get_template_directory_uri().'/css/normalize.css');
