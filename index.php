afb518ca7acc98a66d6d4d62eb7cc5b9406db5b5
Josh <JPollock412@gmail.com>
2013-04-16 03:22:18 -0400

    removing extra spaces from index.php

--- index.php
+++ index.php
@@ -15,12 +15,16 @@ get_header(); ?>
 
 	<div id="primary" class="content-area row">
 		<div id="content" class="site-content large-9 columns" role="main">
-
+		<?php 
+			if ( get_theme_mod( '_s_f_slider_visibility' ) == '' ) { 
+	   		if ( is_front_page() ) : 
+		    get_template_part( 'slider' );
+	    	endif;
+			}
+		?>
 		<?php if ( have_posts() ) : ?>
-
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
-
 				<?php
 					/* Include the Post-Format-specific template for the content.
 					 * If you want to overload this in a child theme then include a file
@@ -28,17 +32,11 @@ get_header(); ?>
 					 */
 					get_template_part( 'content', get_post_format() );
 				?>
-
 			<?php endwhile; ?>
-
 			<?php _sf_content_nav( 'nav-below' ); ?>
-
 		<?php else : ?>
-
 			<?php get_template_part( 'no-results', 'index' ); ?>
-
 		<?php endif; ?>
-
 		</div><!-- #content -->
 	<?php get_sidebar(); ?>
 	</div><!-- #primary -->
