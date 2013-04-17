030ef8909499ecf7aefcdfa691313ba32be86f66
Josh <JPollock412@gmail.com>
2013-04-17 01:55:13 -0400

    Putting some instructions in desc-extra.js

--- js/extra-desc.js
+++ js/extra-desc.js
@@ -1,3 +1,3 @@
 jQuery(document).ready(function($) {
-	$('.theme-description').append('<p>Test</p>');
+	$('.theme-description').append('<p>To change settings for the home page slider, header and menu use the theme customizer.</p><p>For more info please see the <a href="http://complexwaveform.com/jp/sf/documentation/" title="_Second Foundation Documentation">theme documentation</a>.</p>');
 });
