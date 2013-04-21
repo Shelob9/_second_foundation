de37ac81a20eecd2f0e25a40a3dc6087810bef98
Josh <JPollock412@gmail.com>
2013-04-20 20:35:18 -0400

    Revert "move sidebar to the left"
    This reverts commit 49898a7a2b5d00feb2bb33b307f5d547ae6387fc.

--- 404.php
+++ 404.php
@@ -8,7 +8,7 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area row">
-		<div id="content" class="site-content large-9 columns push-3" role="main">
+		<div id="content" class="site-content large-9 columns" role="main">
 
 			<article id="post-0" class="post error404 not-found">
 				<header class="entry-header">
