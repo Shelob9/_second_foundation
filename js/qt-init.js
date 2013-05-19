6b23000ff72c5e6ae8d504f6e6820992ec0315ad
Josh <JPollock412@gmail.com>
2013-05-19 17:55:44 -0400

    simplify animations

--- js/qt-init.js
+++ js/qt-init.js
@@ -7,30 +7,7 @@ var log = function(string) {
 log('center rotate');
 */
 $('#content')
-  .css('translate', 100)
-  .css('rotate', -1)
-  .css({transformOriginX:'100%', transformOriginY:'100%'})
-  .css('transformOrigin', '0%,0%')
-  .delay(1000)
-  
-  .animate({rotate: 0.28}, 900, function(){log('bottom right rotate')})
-  .delay(1000)
-  .animate({transformOriginX:'100%', transformOriginY:'100%'}, 10)
-  
-  .animate({rotate: 2}, 3000, function(){log('translate')})
-  
-  .animate({transformOriginX:'50%', transformOriginY:'50%'}, 1000)
-  .delay(1000)
-  .animate({translate: 200}, 1000, 'swing')
-  .animate({translateY: 0}, 1000)
-  .animate({translateX: 100}, 1000, function(){log('scale')})
-  .delay(1000)
-  .animate({scaleY: 0.3}, 2000)
-  .animate({scaleX: 0.1}, 2000)
-  .animate({scale: 1}, 500, 'swing')
-  .animate({rotate: -0.785}, 2000, 'swing')
-  
-  .animate({scale: 1}, 500, 'swing', function(){log('add rotation and swing origin')})
+ 
   .animate({rotate: '+=90deg', transformOriginX:'0%', transformOriginY:'0%'}, 3000, 'swing')
   
   .delay(300)
