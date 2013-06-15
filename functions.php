01ba030cea31b7f66cfe409d073e555e4cfb107e
Josh <JPollock412@gmail.com>
2013-06-15 18:54:44 -0400

    create template loads in functions.php

--- functions.php
+++ functions.php
@@ -1,51 +1,39 @@
 <?php
 /**
- * Includes various parts of the functions library
+ * Includes various parts of the functions incrary
  *
  * @package _sf
  * revamped 1.0.5
  */
 
+// Will load file from child theme if it exists, if not from parent theme.
+// See: http://codex.wordpress.org/Function_Reference/locate_template
 
-
-/**
- * Include foundation menu functions
- */
-
-require( get_template_directory() . '/inc/foundation.php' );
-
-
-
-
-
-
-/**
-* Use style.php for color options
-*/
-require('css/style.php');
-
-
-
-
-
-/*
-* Conditionally Add Slider For Home Page
-*/
-function _sf_home_slider() { 
-
-	if ( get_theme_mod( '_sf_slider_visibility' ) == '' ) { 
-	if ( is_front_page() ) : 
-	get_template_part( 'slider' );
-	endif;
-	}
-}
+// Theme setup functions.
+locate_template('/inc/theme-setup.php', true);
+// Adds scripts and styles.
+locate_template('/inc/scripts-styles.php', true);
+// Header and menu function.
+locate_template('/inc/header-menu.php', true); 
+// Sets up grid for page layout main content and sidebar areas.
+locate_template('/inc/open-close.php', true);
+// All image related functions including background image and responsive thumbnails.
+locate_template('/inc/image-stuff.php', true); 
+//Adds options to customizer
+locate_template('/inc/customizer.php', true); 
+//Puts styles set in customzier into head.
+locate_template('/css/style.php', true);
+//Functions that act independently of the theme templates from _S.
+locate_template('/inc/extras.php', true); 
+// Custom template tags for this theme from _S.
+locate_template('/inc/template-tags.php', true);
 
 
 
 
-/**
-* include inc/header-maker.php which makes the header
-*/
-include ('inc/header-maker.php');
+
+
+
+
 
 
