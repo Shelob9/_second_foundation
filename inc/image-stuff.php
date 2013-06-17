e5ba1e8a227b881dc518ce9ca4e80e26012f9101
Josh <JPollock412@gmail.com>
2013-06-17 00:54:42 -0400

    remove old full screen background image thing

--- inc/image-stuff.php
+++ inc/image-stuff.php
@@ -24,12 +24,12 @@
 
 
 /**
-* Set the fullscreen background image using a smaller image for small (ie mobile screens)
+* FGt id of attachment by full url
 * http://pippinsplugins.com/retrieve-attachment-id-from-image-url/
+* as of 1.0.5.1 is no longer used. But I'm keeping it beacuse it is neat.
 */
 if (! function_exists('_sf_get_image_id') ) :
-//get id of attachment by full url
-// http://pippinsplugins.com/retrieve-attachment-id-from-image-url/
+
 function _sf_get_image_id($image_url) {
 
 		global $wpdb;
@@ -38,8 +38,6 @@ function _sf_get_image_id($image_url) {
 		return $attachment[0]; 
 }
 endif; // !  _sf_get_image_id exists
-
-// add the script to check screen size and if the height is less then 480 set background image to mobile size IF we are using a fullscreen background.
 
 if (! function_exists('_sf_bg_img_size_decider') ) :
 function _sf_bg_img_size_decider() {
@@ -50,26 +48,6 @@ function _sf_bg_img_size_decider() {
 add_action('wp_enqueue_scripts', '_sf_bg_img_size_decider');
 endif; // ! _sf_bg_img_size_decider exists
  
-
-//function to put the bg img or not into header
-if (! function_exists('_sf_full_bg') ) :
-	function _sf_full_bg() {
-		//if we are using a full screen background image add a container for it with a background image that is sized based on screen height to avoid loading massive file for phones.
-			//First, get the url of the full-size fullscreen background image. Do this before checking if we need it so we can test if any is set.
-			$bg_full = get_theme_mod('body_bg_img');
-			//if we're using full screen background image, and we've set one then continue.
-			if (! get_theme_mod( 'body_bg_choice' ) == '' && ! $bg_full == '') {
-			// store the image ID in a var
-			$bg_img_id = _sf_get_image_id($bg_full);
-			// Set size of full screen background based on window 
-	?>
-	<div id="bg"
-		data-fullbg-src="<?php $image_attributes = wp_get_attachment_image_src( $bg_img_id, 'full' ); echo $image_attributes[0]; ?>"
-		data-smallbg-src="<?php $image_attributes = wp_get_attachment_image_src( $bg_img_id, 'mobile-bg' ); echo $image_attributes[0];  ?>"
-	>
-<?php } 
-}
-endif; // ! _sf_full_bg exists
 
 
 /**
