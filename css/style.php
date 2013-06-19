98f35112dbeeaa0e7ace2bc84b362e2231718e62
Josh <JPollock412@gmail.com>
2013-06-19 03:54:03 -0400

    fix post tile color option

--- css/style.php
+++ css/style.php
@@ -40,7 +40,7 @@ echo '<style>'; ?>
 	.entry-content { color:  <?php echo $content_text_color; ?>; }
 	#content a { color:  <?php echo $content_link_color; ?>; }
 	.site-description {color: <?php echo $site_description_color; ?> }
-	h1.entry-title a {color: <?php echo $post_title_color; ?> }
+	#content h1.entry-title a {color: <?php echo $post_title_color; ?> }
 	.top-bar-section ul li>a {color: <?php echo $menu_text_color; ?> }
 	.top-bar .name h1 a, h1.site-title a {color: <?php echo $site_name_color; ?> }
 	.top-bar, .top-bar-section li a:not(.button) {background-color: <?php echo $menu_bg_color; ?> }
