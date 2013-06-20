d34bf9bd7ab7a3a994c3146a52662cbf40ae3a3e
Josh <JPollock412@gmail.com>
2013-06-20 02:25:30 -0400

    comment reply thing in header

--- header.php
+++ header.php
@@ -20,6 +20,7 @@
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
+ <?php if ( is_singular() ) wp_enqueue_script( "comment-reply" ); ?>
 <?php wp_head(); ?>
 </head>
 
