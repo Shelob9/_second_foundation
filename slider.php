63db1cbb2001edd3502e73f981f6d8f49f16e032
Josh <JPollock412@gmail.com>
2013-06-20 02:09:45 -0400

    return options in style.php, etc

--- slider.php
+++ slider.php
@@ -4,12 +4,14 @@
 		<?php $query = new WP_Query("category_name=".get_theme_mod( '_sf_slide_cat' )."&posts_per_page=".get_theme_mod( '_sf_slide_numb') ); ?>
 	
     	<?php while ($query->have_posts()) : $query->the_post(); ?>
-			  <li>
-				<?php the_post_thumbnail(); ?>
+			<li>
+				<?php get_template_part('content', 'orbit'); ?>
 				<div class="orbit-caption">
-					<span class="orbit-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', '_s' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></span><span class="orbit-excerpt"><?php the_excerpt() ?></a></span>
+					<span class="orbit-title">
+						<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', '_s' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
+					</span>
 				</div>
-			  </li>
+			</li>
 
 	<?php endwhile; ?>
 		<?php endif; ?>
