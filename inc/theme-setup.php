99a8df085447588d260d53851830fb58221a334c
Josh <JPollock412@gmail.com>
2013-06-20 20:45:28 -0400

    make _sf_new_excerpt_more conditional on if we're using masonry

--- inc/theme-setup.php
+++ inc/theme-setup.php
@@ -88,7 +88,9 @@ endif; //! ! _sf_add_class_to_excerpt exists
 
 if (! function_exists('_sf_new_excerpt_more' ) ) :
 function _sf_new_excerpt_more( $more ) {
-	return ' <a class="read-more" href="'. get_permalink( get_the_ID() ) . '">Read More</a>';
+	if (function_exists('_sf_scripts_masonry')) {
+		return ' <a class="read-more" href="'. get_permalink( get_the_ID() ) . '">Read More</a>';
+	}
 }
 add_filter( 'excerpt_more', '_sf_new_excerpt_more' );
 endif; //! _sf_new_excerpt_more exists
@@ -98,7 +100,7 @@ function _sf_custom_excerpt_length( $length ) {
 	//get the value of the masonry excerpt length option, or set it to 10 if !isset
 	$masonry_excerpt_length = get_theme_mod('masonry_excerpt_length', 10);
 	//if masonry functions exists, since we're using it set using $masonry_excerpt_length, else leave at 55.
-	if (function_exists('_sf_init_masonry')) {
+	if (function_exists('_sf_scripts_masonry')) {
 		return $masonry_excerpt_length;
 	}
 	else {
