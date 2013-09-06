b8d3310985a1000022eceecd9aee454f6a9737f2
Josh <JPollock412@gmail.com>
2013-09-06 02:34:52 -0400

    Put thumbnail for masonry into has_post_thumbnail

--- content-masonry.php
+++ content-masonry.php
@@ -1,9 +1,11 @@
 	<?php do_action( 'tha_entry_before' ); ?>
 	<article class="masonry-entry  panel radius" id="post-<?php the_ID(); ?>" <?php post_class(); ?> >
 	<?php do_action( 'tha_entry_top' ); ?>
+	<?php if ( has_post_thumbnail() ) : ?>
         <div class="masonry-thumbnail">
             <a href="<?php the_permalink(' ') ?>" title="<?php the_title(); ?>"><?php the_post_thumbnail('masonry-thumb'); ?></a>
         </div>
+    <?php endif; //has_post_thumbnail ?>
   		<div class="masonry-details">
 			<h5  show-for-medium-up hide-for-small"><a href="<?php the_permalink(' ') ?>" title="<?php the_title(); ?>"><span class="masonry-post-title"> <?php the_title(); ?></span></a></h5>
 			<?php if ( get_theme_mod( '_sf_masonry_excerpt' ) == '' ) :
