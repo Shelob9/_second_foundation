3b0cce4d79b6a17eb9d9ca97228db65855fdcdcc
Josh Pollock <jpollock412@gmail.com>
2013-11-23 20:50:09 -0500

    menu depth of zero

--- sf2/sf2.php
+++ sf2/sf2.php
@@ -95,7 +95,7 @@ class _SF2 {
             'nav_walker.php',
             'foundation.php',
             'fgrid.php',
-            'header-stuff.php'
+            //'header-stuff.php'
         );
         foreach ($files as $file) {
             require_once( trailingslashit( _SF2_CLASSES ) . $file );
