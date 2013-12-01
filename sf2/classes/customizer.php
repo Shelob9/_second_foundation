c7f24189f54603a989198c4d08625b6cb8ed55ac
Josh Pollock <jpollock412@gmail.com>
2013-12-01 11:18:08 -0500

    add back customizer so it can control bg

--- sf2/classes/customizer.php
+++ sf2/classes/customizer.php
@@ -13,7 +13,7 @@
 
 
     /**
-     * Add nav options to customizer
+     * Add options to customizer
      *
      * @author Josh Pollock
      * @package _Second Foundation
@@ -21,50 +21,5 @@
      */
     function _sf_customizer() {
         global $wp_customize;
-        /**
-         * Add Nav Options Section '_sf_nav_options'
-         */
-        //name section in a var
-        $section = '_sf_nav_options';
-        $wp_customize->add_section( $section, array(
-            'title'          => __( 'Navigation Options', '_sf' ),
-            'priority'       => 35,
-        ) );
-
-        /**
-         * Settings and controls for Nav Options
-         */
-        $navs = array(
-            //Slideout menu instead of topbar?
-                array(
-                    'setting'   => 'alt_nav_only',
-                    'default'   => true,
-                    'label'     => 'Use slideout menu instead of topbar?',
-                ),
-            //Alt nav only on mobile
-                array(
-                    'setting'   => 'alt_nav_mobile',
-                    'default'   => true,
-                    'label'     => 'Sliedout menu for mobile only?',
-                ),
-
-        );
-
-        /**
-         * Create Settings and Controls For Nav Options In A Handy Loop
-         */
-        foreach ( $navs as $nav ){
-            $wp_customize->add_setting($nav['setting'],
-                array(
-                    'default' => $nav['default'],
-                )
-            );
-            $wp_customize->add_control( $nav['setting'],
-                array(
-                    'label'     => __($nav['label'], 'sf'),
-                    'section'   => $section,
-                    'type'      => 'checkbox',
-                )
-            );
-        }
+    
     }
