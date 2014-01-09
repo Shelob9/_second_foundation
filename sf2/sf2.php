a082ccbd0ec576e710fbe4558c2ebb56dcfc2336
Josh Pollock <jpollock412@gmail.com>
2014-01-08 19:41:27 -0500

    add method for smart template part.

--- sf2/sf2.php
+++ sf2/sf2.php
@@ -138,6 +138,42 @@ class _SF2 {
             return false;
         }
     }
+
+    /**
+     * Smart template part. Use in place of get_template_part. Uses pods_view for caching if Pods (http://Pods.io) is installed and activated.
+     *
+     * @param   array $parts File path. Passed to get_template_part() or pods_view( $view).
+     * @param   bool|array  $cache (optional) Array of caching options for pods_view, false to not cache.
+     *  @args
+     *          string   $expire Length Time in seconds for the cache to expire, if false caching is disabled.
+     *          string  $mode The caching method to use -- cache, transient, or site-transient.
+     *
+     * @author Josh Pollock
+     * @package _sf
+     * @since 2.0.2
+     *
+     * @uses pods_view
+     */
+    public function temp_part( $parts, $cache = false ) {
+        //use get_template_part() if Pods !installed
+        if ( ! function_exists( 'pods' ) ) {
+            get_template_part( $parts[0], $parts[1] );
+        }
+        else {
+            //set up $view from $parts
+            $view = $parts[0];
+            if ( isset( $parts[1] ) ) {
+                $view .= '/'.$parts[1];
+            }
+            //if we are caching sort args for it
+            if ( $cache != false ) {
+                $expires = $cache[ 'expire' ];
+                $cache_mode = $cache[ 'mode' ];
+            }
+            //make it so with pods_view()
+            pods_view( $view, $data = null, $expires, $cache_mode, $return = false );
+        }
+    }
 }
 
 /**
