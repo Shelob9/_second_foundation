b9533ad8df1b6dfae75596d4ae5fe1d4f6cad7b3
Josh Pollock <jpollock412@gmail.com>
2014-01-19 18:35:41 -0500

    fix pushIt arg on line 45

--- sf2/js/sf2.js
+++ sf2/js/sf2.js
@@ -42,7 +42,7 @@ jQuery(document).ready(function($) {
         var pushIt = function ( push ) {
             $(main).css({marginTop: push + 'px' });
         };
-        pushIt();
+        pushIt( $(header).height() );
         /* On scroll away from top hide #masthead and put its background onto the topbar */
         //define the behaviour when scrolled away
         var away = function () {
